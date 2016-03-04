d6dcb6d0de308962235cccc0ab2863d6177e6150
Christine <Christine@Christines-MacBook-Air.local>
2016-03-04 10:50:59 -0500

    Fully functioning program

--- tenCharacter.py
+++ tenCharacter.py
@@ -1,7 +1,17 @@
 f = open("input.txt", 'r')
-input = f.read()
+input = f.readlines()
 f.close()
 
 f1 = open ("output.txt", 'w')
-f1.write(input)
-f1.close()
+
+for characters in input:
+	if len(characters) <= 10: #doesn't need it because \n already in input
+		f1.write(characters)
+		
+	else:
+		f1.write(characters[0:10])
+		f1.write("\n") #only when characters more than 10 bc chopping off the new line too
+		
+	
+
+		
